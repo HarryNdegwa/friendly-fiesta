86d8b99062502a1a7c673484e53108e9303746fe
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-10-05 00:51:29 +0300

    Added users routers

---
+++ routes/users.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.post("/login", async (req, res) => {});
+router.post("/register", async (req, res) => {});
+
+export default router;
